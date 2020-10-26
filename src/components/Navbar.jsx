d12624ffb23ad0b51b72034bf96ecb11d03d7e58
Agu Maxwell Chigozie <agumaxwell9@gmail.com>
2020-10-26 09:58:23 +0100

    Fixed heroku https restriction

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,7 @@ export default class Navbar extends Component {
                           name="searchParams"
                           id="search"
                           placeholder="Enter search term (e.g iphone x, 128Gb or A1)"
-                          required
+                          // required
                           value={this.state.search_value}
                           onChange={this.handleChange.bind(this)}
                         ></input>
